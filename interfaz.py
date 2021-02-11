c167bcfdd39fcede9b27e239fa77e04784813177
unknown <tobi.mangali@gmail.com>
2021-02-11 15:53:02 -0300

    Cambio de codigo y empiezo a usar la libreria Instaloader hasta que solucione bugs

--- interfaz.py
+++ interfaz.py
@@ -1,91 +1,41 @@
 from tkinter import *
 from PIL import ImageTk, Image
+import instaloader
 
 root = Tk()
 root.title("InstaScrap")
 root.iconbitmap("C:/Users/tobi/Desktop/Proyectos/tkinter/imagenes/instagram1.ico")
 
-#Tengo que ver como pasarlo a otro archivo, importarlo y que me deje usarlo
-from time import sleep
-from selenium import webdriver
-import time
-
-seguidores=[]
-seguidos = []
-#Abro chrome y voy a instagram
 def analizar(Usuario,Contraseña,Cuenta):
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    browser.get('https://www.instagram.com/')
+    L = instaloader.Instaloader()
+    # Login or load session
+    L.login(Usuario, Contraseña)        # (login)
+    # Obtain profile metadata
+    profile = instaloader.Profile.from_username(L.context, Cuenta)
 
-    #Esto es para hacer el programa que labure en segundo plano, pero tiene bugs
-    '''options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-    browser = webdriver.Chrome(chrome_options=options)
-    browser.set_window_size(1980, 1080)
-    browser.get('https://www.instagram.com/')'''
-    sleep(2)
-    #Me logeo a mi cuenta
-    usuario_input = browser.find_element_by_css_selector("input[name='username']")
-    contraseña_input = browser.find_element_by_css_selector("input[name='password']")
-    usuario_input.send_keys(Usuario)
-    contraseña_input.send_keys(Contraseña)
-    IniciarSesion = browser.find_element_by_xpath("//button[@type='submit']")
-    IniciarSesion.click()
-    sleep(5)
-    #voy hasta el perfil
-    cuenta = Cuenta
-    browser.get("https://www.instagram.com/"+ cuenta +"/?hl=es-la")
-    #cantidad de seguidores y seguidos
-    cantSeguidos = int(browser.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[3]/a/span").text) - 1
-    cantSeguidores = int(browser.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span").text) - 1
-    #busco los seguidores
-    seguidores2 = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
-    seguidores2.click()
-
-
-    #Esto puede llegar a optimizar el programa pero tiene bugs
-    '''cantSeguidoresEnteros = cantSeguidores // 12
-    for i in range(12,cantSeguidoresEnteros,12):
-    scr1 = browser.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li[%s]' % i)
-    browser.execute_script("arguments[0].scrollIntoView();", scr1)
-    sleep(0.01)'''
-
-    
-    for i in range(1,cantSeguidores):
-        scr1 = browser.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li[%s]' % i)
-        browser.execute_script("arguments[0].scrollIntoView();", scr1)
-        sleep(0.001)
-        text = scr1.text
-        lista = text.split()
-        seguidores.append(lista[0])
-
-    browser.refresh()
-
-    sleep(2)
-    #busco los seguidos
-    seguidos2 = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a')
-    seguidos2.click()
-    
-    for i in range(1,cantSeguidos):
-        scr1 = browser.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li[%s]' % i)
-        browser.execute_script("arguments[0].scrollIntoView();", scr1)
-        sleep(0.005)
-        text = scr1.text
-        lista = text.split()
-        seguidos.append(lista[0])
-
-    browser.quit()
+    # Print list of followees
+    lista_Seguidores = []
+    for seguidor in profile.get_followers():
+        lista_Seguidores.append(seguidor.username)
+    lista_Seguidos = []
+    for seguido in profile.get_followees():
+        lista_Seguidos.append(seguido.username)
 
     #Esta es la parte que comparo las listas
-    seguidoresMutuos = set(seguidores).intersection(seguidos)
-    noSeguidores = list(set(seguidos)-seguidoresMutuos)
-    fans = list(set(seguidores) - seguidoresMutuos)
-    #noSeguidores = list(noSeguidores2)
+    seguidoresMutuos = set(lista_Seguidos).intersection(lista_Seguidores)
+    noSeguidores = list(set(lista_Seguidos) - seguidoresMutuos)
+    fans = list(set(lista_Seguidores) - seguidoresMutuos)
+    
     #abro nueva ventana con los resultados
     ventanaAnalisis = Toplevel()
+
     #titulos
-    
+    frameTitulos = Frame(ventanaAnalisis)
+    frameTitulos.pack(side = "top")
+    fanes = Label(frameTitulos, text = "Fans")
+    noFollowers = Label(frameTitulos, text = "No seguidores")
+    noFollowers.pack(side = "left",padx = 50,expand = "YES", fill = "x")
+    fanes.pack(side = "right",padx = 50, expand = "YES",fill = "x")
     #hago display de las listas en la ventana
     #Slider de las listas
     verticalBar = Scrollbar(ventanaAnalisis)
@@ -104,6 +54,7 @@ def analizar(Usuario,Contraseña,Cuenta):
     verticalBar.config(command = listaNoSeguidores.yview)
     verticalBar2.config(command = listaFans.yview)
 
+
     
 
 
@@ -119,11 +70,14 @@ def abrirVentanaPrincipal():
     nuevaVentana = Toplevel()
     nuevaVentana.geometry("400x400")
     #DEF
-    botonAnalizar = Button(nuevaVentana,padx = 30,pady = 15, text = "Analizar seguidores",command = lambda : analizar(usuario,contraseña,"sofiviera3") )
+    botonAnalizar = Button(nuevaVentana,padx = 30,pady = 15, text = "Analizar seguidores",command = lambda : analizar(usuario,contraseña,"santiferreyra_") )
     botonBuscar = Button(nuevaVentana,padx = 30,pady = 15, text = "¿Vio mi historia?")
     #Display
     botonAnalizar.grid(row = 0, column = 0,padx = 120,pady = 100)
     botonBuscar.grid(row = 1, column = 0,padx= 120, pady = 0)
+    #Cierro la ventana Login
+    nuevaVentana.mainloop()
+    root.destroy()
 
 
 
